f40071db7ceb4607c5b3c5a987dbb0776fb7dbe5
Nada_Yasir <167257640+NadaYasir2@users.noreply.github.com>
2024-04-18 21:36:20 +0300

    Update views.py

--- reservation/clients/views.py
+++ reservation/clients/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1> Welcome 😊 </h1>")
+
+def header(request):
+    return render(request, 'header.html')
+
+def add (requst):
+    return render(requst, 'add.html')
