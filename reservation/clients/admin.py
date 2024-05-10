a5fb87b2a73b9e5cbaf80f051916240ddacdacfd
Nada-Abumandil <134234575+Nada-Abumandil@users.noreply.github.com>
2024-05-10 03:45:11 +0300

    Initial commit with models

--- reservation/clients/admin.py
+++ reservation/clients/admin.py
@@ -1,3 +1,11 @@
+# Import the admin module
 from django.contrib import admin
 
-# Register your models here.
+# Import your models
+from .models import Client, ClientType, Product, Order
+
+# Register your models to make them available in the admin interface
+admin.site.register(Client)
+admin.site.register(ClientType)
+admin.site.register(Product)
+admin.site.register(Order)
--- reservation/clients/models.py
+++ reservation/clients/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class ClientType(models.Model):
+    type_id = models.IntegerField(primary_key=True, default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    GENDER_CHOICES = [(1, "Male"), (2, "Female")]
+
+    cid = models.IntegerField(primary_key=True, default=0)
+    name = models.CharField(max_length=150, null=False)
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField()
+    type = models.ForeignKey(ClientType, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    pid = models.IntegerField(primary_key=True, default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    oid = models.IntegerField(primary_key=True, default=0)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_date = models.DateField()
+    shipping_date = models.DateField()
